53d6f6f924d4630b2ad9bfc8e345752647a3048c
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-06 12:44:58 -0600

    Ya se pueden agregar nuevas sucursales, solo faltan los reportes

--- Biblioteca.java
+++ Biblioteca.java
@@ -4,22 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
+    private String nameSucursal;
     private int sucursal;
     private List<Biblioteca> sucursales;
+
 
     public Biblioteca(){
         this.sucursales = new ArrayList<>();
     }
 
-    public Biblioteca(int sucursal){
+    public Biblioteca(String nameSucursal,int sucursal){
+        this.nameSucursal = nameSucursal;
         this.sucursal = sucursal;
     }
 
     public void generarReporteGeneroMasSolicitado(String filesucursal){
 
     }
-    public void agregarSucursales(int sucursal){
-        Biblioteca sucursalnueva = new Biblioteca(sucursal);
+    public void agregarSucursales(String nameSucursal){
+        sucursal = sucursales.size() + 1;
+        Biblioteca sucursalnueva = new Biblioteca(nameSucursal, sucursal);
         sucursales.add(sucursalnueva);
+    }
+
+    public List<Biblioteca> getSucursales(){
+        return sucursales;
     }
 }
