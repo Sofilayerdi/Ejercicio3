e4d5cc639b636ea678134631375a6794903d2a65
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-06 10:56:31 -0600

    Clase biblioteca, aun le falta

---
+++ Biblioteca.java
@@ -0,0 +1,13 @@
+package Ejercicio_3.Ejercicio3;
+
+public class Biblioteca {
+    int sucursal;
+
+    public Biblioteca(int sucursal){
+        this.sucursal = sucursal;
+    }
+
+    public void generarReporte(String filesucursal){
+
+    }
+}
