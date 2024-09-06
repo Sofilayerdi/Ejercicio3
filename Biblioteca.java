dfad856e416e2c9ef5e20c672410e2ea39e94ed8
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-06 12:16:10 -0600

    Ya esta agregado para aniadir nuevas sucursales

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,31 +1,25 @@
-<<<<<<< HEAD
 package Ejercicio_3.Ejercicio3;
 
 import java.util.ArrayList;
 import java.util.List;
 
-=======
->>>>>>> d183d016443ce89da240377c2c8d581b897850a1
 public class Biblioteca {
     private int sucursal;
-    private List<Libro> libros;
-    private List<Miembro> prestamos;
+    private List<Biblioteca> sucursales;
+
+    public Biblioteca(){
+        this.sucursales = new ArrayList<>();
+    }
 
     public Biblioteca(int sucursal){
         this.sucursal = sucursal;
-        this.libros = new ArrayList<>();
-        this.prestamos = new ArrayList<>();
-    }
-
-    public void agregarLibro(Libro libro){
-        libros.add(libro);
     }
 
     public void generarReporteGeneroMasSolicitado(String filesucursal){
 
     }
-
-    public int getSucursal(){
-        return sucursal;
+    public void agregarSucursales(int sucursal){
+        Biblioteca sucursalnueva = new Biblioteca(sucursal);
+        sucursales.add(sucursalnueva);
     }
 }
