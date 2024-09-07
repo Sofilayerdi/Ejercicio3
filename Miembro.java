a11723a28685a432ecc2e0fe24b4b59b7d407774
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-06 22:31:38 -0600

    Modifique un poco

--- Miembro.java
+++ Miembro.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -9,8 +6,8 @@ public class Miembro {
     private String nombre;
     private LocalDate fechaP; 
     private LocalDate fechaD; 
-    private ArrayList<String> prestamosActivos; 
-    private ArrayList<String> prestamos;
+    private ArrayList<Integer> prestamosActivos; 
+    private ArrayList<Integer> prestamos;
 
     public Miembro(int ID, String nombre) {
         this.ID = ID;
@@ -37,47 +34,32 @@ public class Miembro {
         return fechaD;
     }
 
-    public ArrayList<String> getPrestamos() {
+    public ArrayList<Integer> getPrestamos() {
         return prestamos;
     }
 
-    public ArrayList<String> getPrestamosActivos() {
+    public ArrayList<Integer> getPrestamosActivos() {
         return prestamosActivos;
     }
 
-    public void solicitarLibro(String libroID, LocalDate fechaP) {
-        this.prestamos.add(libroID);
-        this.prestamosActivos.add(libroID);
+    public void solicitarLibro(int isbn, LocalDate fechaP) {
+        this.prestamos.add(isbn);
+        this.prestamosActivos.add(isbn);
         this.fechaP = fechaP;
         this.fechaD = fechaP.plusDays(30);
     }
 
-    public void devolucion(String libroID) {
-        this.prestamosActivos.remove(libroID);
+    public void devolucion(int isbn) {
+        this.prestamosActivos.remove((Integer) isbn);
     }
 
-    public String buscarLibro(int sucursal, String libroID, String rutaCSV) {
-        String nombreLibro = null;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(rutaCSV))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                String b_sucursal = parts[5];
-
-                if (b_sucursal.equals(String.valueOf(sucursal))) {
-                    String isbn = parts[0];
-                    if (isbn.equals(libroID)) {
-                        nombreLibro = parts[1];
-                        break;
-                    }
-                }
+    public ArrayList<Integer> getLibrosPrestadosMes(LocalDate mes) {
+        ArrayList<Integer> librosMes = new ArrayList<>();
+        for (int isbn : prestamos) {
+            if (fechaP != null && fechaP.getMonth().equals(mes.getMonth()) && fechaP.getYear() == mes.getYear()) {
+                librosMes.add(isbn);
             }
-        } catch (IOException e) {
-            System.out.println("Error: No se pudo leer el archivo CSV.");
         }
-
-        return nombreLibro;
+        return librosMes;
     }
 }
-
