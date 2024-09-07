e713fe63744920c00b78c9c9c4a985d136c93b6a
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-06 21:54:47 -0600

    Sin modificaciones

--- Miembro.java
+++ Miembro.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
-;
+import java.util.ArrayList;
 
 public class Miembro {
     private int ID;
@@ -26,7 +25,6 @@ public class Miembro {
         return ID;
     }
 
-
     public String getNombre() {
         return nombre;
     }
@@ -47,47 +45,39 @@ public class Miembro {
         return prestamosActivos;
     }
 
-
     public void solicitarLibro(String libroID, LocalDate fechaP) {
         this.prestamos.add(libroID);
         this.prestamosActivos.add(libroID);
         this.fechaP = fechaP;
         this.fechaD = fechaP.plusDays(30);
-
     }
 
     public void devolucion(String libroID) {
-        if (this.prestamosActivos.contains(libroID)) {
-            this.prestamosActivos.remove(libroID);
-        }
-
+        this.prestamosActivos.remove(libroID);
     }
 
     public String buscarLibro(int sucursal, String libroID, String rutaCSV) {
         String nombreLibro = null;
-        boolean found = false;
 
-            BufferedReader reader = new BufferedReader(new FileReader(rutaCSV));
+        try (BufferedReader reader = new BufferedReader(new FileReader(rutaCSV))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                String b_sucursal = parts[5]; 
+                String b_sucursal = parts[5];
 
-                if (b_sucursal.equals(sucursal)) {
+                if (b_sucursal.equals(String.valueOf(sucursal))) {
                     String isbn = parts[0];
-                    if(isbn.equals(libroID)){
-                        found = true;
-                        nombreLibro = parts[1]; 
+                    if (isbn.equals(libroID)) {
+                        nombreLibro = parts[1];
                         break;
                     }
                 }
             }
-
-        if (found) {
-            return nombreLibro;
-        } else {
-            return null;
+        } catch (IOException e) {
+            System.out.println("Error: No se pudo leer el archivo CSV.");
         }
+
+        return nombreLibro;
     }
 }
+
